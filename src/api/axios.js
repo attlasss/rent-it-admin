5077482985c04a0d53aeaa7a7de9a54c48b85c28
carolina <carolynmortoby@gmail.com>
2025-05-10 23:08:17 +0200

    a

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 let axiosConn = axios.create({
-  baseURL: "https://rentit-backend-ci8qak394-carolinamv1234s-projects.vercel.app",
- 
+  baseURL: "https://rentit-backend-beige.vercel.app",
 });
 
 export default axiosConn;
-
-
